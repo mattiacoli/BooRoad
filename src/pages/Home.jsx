07752c0176fbd7a3b0cd9ade294809af2ad9dfdb
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-04-24 15:17:28 +0200

    import data array in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import gite from '../data/data'
+import { Link } from 'react-router-dom'
+
 export default function Home() {
     return (
         <>
-            <h1>home</h1>
+
         </>
     )
 }
