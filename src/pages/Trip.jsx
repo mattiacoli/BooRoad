e1569fca35dd7e462f2d9f394cd719fae597cf15
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-04-24 17:39:14 +0200

    Add User component and implement user search functionality in Trip page

--- src/pages/Trip.jsx
+++ src/pages/Trip.jsx
@@ -1,5 +1,7 @@
 import data from '../data/data.js'
 import { useParams } from 'react-router-dom'
+import { useState } from 'react';
+import User from '../components/User.jsx'
 
 export default function Trip() {
 
@@ -16,6 +18,19 @@ export default function Trip() {
 
     console.log(tripName);
 
+    const [searchUser, setSearchUser] = useState("")
+    const [UserList, setUserList] = useState(tripName.partecipanti)
+
+    const handleSearch = (e) => {
+        const searchValue = e.target.value.toLowerCase()
+        setSearchUser(searchValue)
+
+        const filteredUsers = tripName.partecipanti.filter(user => {
+            return user.nome.toLowerCase().includes(searchValue) || user.cognome.toLowerCase().includes(searchValue)
+        })
+
+        setUserList(filteredUsers)
+    }
 
     return (
 
@@ -28,6 +43,27 @@ export default function Trip() {
                 <p>🗓️ {tripName.dataInizio} - {tripName.dataFine}</p>
                 <p>📍 Location: {tripName.città}</p>
 
+            </div>
+
+            <div className="container">
+                <h4>Ricerca Partecipanti</h4>
+
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Cerca Partecipante"
+                    value={searchUser}
+                    onChange={handleSearch}
+                />
+
+                <ul className="list-group mt-3">
+                    {UserList.map(user => (
+                        <User
+                            key={user.id}
+                            user={user}
+                        />
+                    ))}
+                </ul>
             </div>
 
         </>
