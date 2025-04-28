6bd6a07df2e64d092d9ae05d5ad690aa5d7f3283
mattiacoli <mattia.coli@aol.com>
2025-04-28 17:39:34 +0200

    Refactor SearchBar and TripTable components for improved layout and styling

--- src/components/TripTable.jsx
+++ src/components/TripTable.jsx
@@ -2,63 +2,57 @@ import { useTripContext } from "../contexts/TripContext.jsx";
 
 export default function TripTable() {
 
-    const { data, searchQuery } = useTripContext()
+  const { data, searchQuery } = useTripContext()
 
-    // Filtra i viaggi in base alla query di ricerca
-    const filteredTrips = data.filter(gita =>
-        gita.nome.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        gita.città.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+  // Filtra i viaggi in base alla query di ricerca
+  const filteredTrips = data.filter(gita =>
+    gita.nome.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    gita.città.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
-    return (
+  return (
 
-        <>
+    <>
+      <div className="container">
 
-            <div className="table-responsive">
-                <h1 className="my-3">Elenco Viaggi</h1>
-                <hr />
+        <div className="table-responsive">
+          <h1 className="my-3">Elenco Viaggi</h1>
+          <hr />
 
-                <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-                    <thead className="table table-sm table-striped">
-                        <tr>
-                            <th>Immagine</th>
-                            <th>Nome</th>
-                            <th>Città</th>
-                            <th>Data Inizio</th>
-                            <th>Data Fine</th>
-                            <th>Partecipanti</th>
-                        </tr>
-                    </thead>
-                    <tbody className="table table-sm table-striped">
-                        {
-                            filteredTrips.map((trip) => {
-                                return (
-                                    <tr key={trip.id}>
-                                        <td>
-                                            <img src={trip.immagine}
-                                                alt="travel image"
-                                                style={{ width: '200px', height: '200px', objectFit: 'cover' }} />
-                                        </td>
-                                        <td>{trip.nome}</td>
-                                        <td>{trip.città}</td>
-                                        <td>{trip.dataInizio}</td>
-                                        <td>{trip.dataFine}</td>
-                                        <td>{trip.partecipanti.length}</td>
-                                    </tr>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
-
-
-
-        </>
-
-
-
-    )
-
-
+          <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+            <thead className="table table-sm table-striped">
+              <tr>
+                <th>Immagine</th>
+                <th>Nome</th>
+                <th>Città</th>
+                <th>Data Inizio</th>
+                <th>Data Fine</th>
+                <th className="text-center">Partecipanti</th>
+              </tr>
+            </thead>
+            <tbody className="table table-sm table-striped">
+              {
+                filteredTrips.map((trip) => {
+                  return (
+                    <tr key={trip.id}>
+                      <td>
+                        <img src={trip.immagine}
+                          alt="travel image"
+                          style={{ width: '80px', height: '50px' }} />
+                      </td>
+                      <td>{trip.nome}</td>
+                      <td>{trip.città}</td>
+                      <td>{trip.dataInizio}</td>
+                      <td>{trip.dataFine}</td>
+                      <td className="text-center">{trip.partecipanti.length}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  )
 }
