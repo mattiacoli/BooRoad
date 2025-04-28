bdcf09f03076c2bc3153d8210b5c193ced513aa1
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-04-28 17:07:04 +0200

    Enhance user data retrieval and emergency contact handling in EditUser component

--- src/pages/EditUser.jsx
+++ src/pages/EditUser.jsx
@@ -5,15 +5,41 @@ import { useState } from "react"
 export default function EditUser() {
 
     const { id } = useParams()
-    const { data } = useTripContext()
+    const { data, setData } = useTripContext()
 
     const [user, setUser] = useState(findUser())
-    const [emergency, setEmergency] = useState(user.contattoEmergenza)
+    const [emergency, setEmergency] = useState(findEmergency())
 
     //find the user by creating a users flat array and comparing user id
     function findUser() {
-        const user = data.flatMap(item => item.partecipanti).find(user => user.id == id)
-        return user
+        const usersList = data.flatMap(item => item.partecipanti)
+        const userFound = {
+            id: usersList.find(user => user.id == id).id,
+            nome: usersList.find(user => user.id == id).nome,
+            cognome: usersList.find(user => user.id == id).cognome,
+            codiceFiscale: usersList.find(user => user.id == id).codiceFiscale,
+            email: usersList.find(user => user.id == id).email,
+            telefono: usersList.find(user => user.id == id).telefono,
+        }
+        return userFound
+    }
+
+    function findEmergency() {
+        const usersList = data.flatMap(item => item.partecipanti)
+        console.log(usersList);
+
+        const emergencyFound = usersList.find(user => user.id == id).contattoEmergenza;
+
+        console.log(emergencyFound);
+
+        return emergencyFound
+    }
+
+    function findTripByUserId(userId) {
+        const trip = data.find(trip =>
+            trip.partecipanti.some(user => user.id == userId)
+        );
+        return trip;
     }
 
     console.log(user);
@@ -39,6 +65,33 @@ export default function EditUser() {
 
     function handleSubmit() {
         console.log('submit');
+
+
+
+        const userToPush = {
+            ...user,
+            contattoEmergenza: emergency
+        }
+
+        const userTrip = findTripByUserId(userToPush.id)
+
+        if (!userTrip) {
+            console.log('nessun viaggio trovaot per l"utente');
+            return
+        }
+
+        const updatedTrip = {
+            ...userTrip,
+            partecipanti: userTrip.partecipanti.map(user => user.id == userToPush.id ? userToPush : user)
+        }
+
+        const updatedData = data.map(trip => trip.id == updatedTrip.id ? updatedTrip : trip)
+
+        setData(updatedData);
+
+        console.log(userToPush);
+
+        console.log(data);
 
     }
 
