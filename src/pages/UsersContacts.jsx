57721009453c2dc977888a5263cfbdf378eb0369
mattiacoli <mattia.coli@aol.com>
2025-04-28 11:46:38 +0200

    Implement sorting functionality for UsersContacts table

--- src/pages/UsersContacts.jsx
+++ src/pages/UsersContacts.jsx
@@ -5,9 +5,66 @@ export default function UsersContacts() {
 
   const { data } = useTripContext()
 
+  const [sortDirection, setSortDirection] = useState("asc")
 
+  function handleSort() {
+    setSortDirection(prev => prev === "asc" ? "desc" : "asc")
+  }
 
-  return (
+  return (<>
     <h1>Elenco Utenti</h1>
+
+    <div
+      className="table-responsive"
+    >
+      <table
+        className="table table-primary"
+      >
+        <thead>
+          <tr>
+            <th scope="col"
+              onClick={handleSort}
+              style={{ cursor: 'pointer' }}>Cognome {sortDirection === "asc" ? "🔼" : "🔽"}</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Mail</th>
+            <th scope="col">Telefono</th>
+            <th scope="col">Codice Fiscale</th>
+            <th scope="col">Contatto di Emergenza</th>
+            <th scope="col">Relazione</th>
+            <th scope="col">Telefono Emergenza</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {data.flatMap((trip) => {
+            trip.partecipanti
+              .sort((a, b) => {
+                if (sortDirection === "asc") {
+                  return a.cognome.localeCompare(b.cognome);
+                } else {
+                  return b.cognome.localeCompare(a.cognome);
+                }
+              })
+              .map((user) => {
+                return (
+                  <tr key={user.id}>
+                    <td>{user.cognome}</td>
+                    <td scope="row">{user.nome}</td>
+                    <td>{user.email}</td>
+                    <td>{user.telefono}</td>
+                    <td>{user.codiceFiscale}</td>
+                    <td>{user.contattoEmergenza.nome}</td>
+                    <td>{user.contattoEmergenza.relazione}</td>
+                    <td>{user.contattoEmergenza.telefono}</td>
+                  </tr>
+                );
+              });
+          })}
+        </tbody>
+      </table>
+    </div >
+
+  </>
+
   )
 }
