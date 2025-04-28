ee76bf24a27f78a64d78c18c377a4d994551d5e9
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-04-28 19:07:58 +0200

    Refactor Companions component and update Trip page to use new accordion structure for displaying accompanying users

--- src/components/Companions.jsx
+++ src/components/Companions.jsx
@@ -1,29 +1,35 @@
 export default function Companions({ nome, cognome, email, telefono, id }) {
     return (
         <>
-            <div class="accordion">
-                <div class="accordion-item">
-                    <h2 class="accordion-header">
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target={`#coll-${id}`} aria-expanded="true">
-                            {nome} {cognome}
-                        </button>
-                    </h2>
-                    <div id={`coll-${id}`} class="accordion-collapse collapse show" data-bs-parent={`#coll-${id}`}>
-                        <div class="accordion-body">
-                            <ul>
-                                <li>
-                                    <span>Email:</span>
-                                    <p>{email}</p>
-                                </li>
-                                <li>
-                                    <span>Telefono:</span>
-                                    <p>{telefono}</p>
-                                </li>
-                            </ul>
-                        </div>
+
+            <div className="accordion-item">
+                <h2 className="accordion-header">
+                    <button className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#comp-${id}`}>
+                        {nome} {cognome}
+                    </button>
+                </h2>
+                <div
+                    id={`comp-${id}`}
+                    className="accordion-collapse collapse show" data-bs-parent="#compAccordion"
+                >
+                    <div className="accordion-body">
+                        <ul>
+                            <li>
+                                <span>Email:</span>
+                                <p>{email}</p>
+                            </li>
+                            <li>
+                                <span>Telefono:</span>
+                                <p>{telefono}</p>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-            </div >
+            </div>
+
 
         </>
     )
