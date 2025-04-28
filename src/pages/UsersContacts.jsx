9da72437e708f429191ec5fb8c91cb916afb055a
mattiacoli <mattia.coli@aol.com>
2025-04-28 12:02:09 +0200

    Refactor UsersContacts component for improved readability and maintainability

--- src/pages/UsersContacts.jsx
+++ src/pages/UsersContacts.jsx
@@ -11,59 +11,61 @@ export default function UsersContacts() {
     setSortDirection(prev => prev === "asc" ? "desc" : "asc")
   }
 
-  return (<>
-    <h1>Elenco Utenti</h1>
+  return (
+    <div className="container mt-4">
+      <h1 className="my-3">Elenco Utenti</h1>
+      <hr />
 
-    <div
-      className="table-responsive"
-    >
-      <table
-        className="table table-primary"
+      <div
+        className="table-responsive"
       >
-        <thead>
-          <tr>
-            <th scope="col"
-              onClick={handleSort}
-              style={{ cursor: 'pointer' }}>Cognome {sortDirection === "asc" ? <i className="bi bi-chevron-down"></i> : <i className="bi bi-chevron-up"></i>}</th>
-            <th scope="col">Nome</th>
-            <th scope="col">Mail</th>
-            <th scope="col">Telefono</th>
-            <th scope="col">Codice Fiscale</th>
-            <th scope="col">Contatto di Emergenza</th>
-            <th scope="col">Relazione</th>
-            <th scope="col">Telefono Emergenza</th>
-          </tr>
-        </thead>
-        <tbody>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col"
+                onClick={handleSort}
+                style={{ cursor: 'pointer' }}>Cognome {sortDirection === "asc" ? <i className="bi bi-chevron-down"></i> : <i className="bi bi-chevron-up"></i>}</th>
+              <th scope="col">Nome</th>
+              <th scope="col">Mail</th>
+              <th scope="col">Telefono</th>
+              <th scope="col">Codice Fiscale</th>
+              <th scope="col">Contatto di Emergenza</th>
+              <th scope="col">Relazione</th>
+              <th scope="col">Telefono Emergenza</th>
+            </tr>
+          </thead>
+          <tbody>
 
-          {data
-            .flatMap((trip) => trip.partecipanti)
-            .sort((a, b) => {
-              if (sortDirection === "asc") {
-                return a.cognome.localeCompare(b.cognome);
-              } else {
-                return b.cognome.localeCompare(a.cognome);
-              }
-            })
-            .map((user) => {
-              return (
-                <tr key={user.id}>
-                  <td>{user.cognome}</td>
-                  <td scope="row">{user.nome}</td>
-                  <td>{user.email}</td>
-                  <td>{user.telefono}</td>
-                  <td>{user.codiceFiscale}</td>
-                  <td>{user.contattoEmergenza.nome}</td>
-                  <td>{user.contattoEmergenza.relazione}</td>
-                  <td>{user.contattoEmergenza.telefono}</td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
-    </div >
+            {data
+              .flatMap((trip) => trip.partecipanti)
+              .sort((a, b) => {
+                if (sortDirection === "asc") {
+                  return a.cognome.localeCompare(b.cognome);
+                } else {
+                  return b.cognome.localeCompare(a.cognome);
+                }
+              })
+              .map((user) => {
+                return (
+                  <tr key={user.id}>
+                    <td>{user.cognome}</td>
+                    <td scope="row">{user.nome}</td>
+                    <td>{user.email}</td>
+                    <td>{user.telefono}</td>
+                    <td>{user.codiceFiscale}</td>
+                    <td>{user.contattoEmergenza.nome}</td>
+                    <td>{user.contattoEmergenza.relazione}</td>
+                    <td>{user.contattoEmergenza.telefono}</td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div >
 
-  </>
+    </div>
+
+
 
   )
 }
