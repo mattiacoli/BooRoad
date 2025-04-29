ae2ded7dceef424aa45c41e14d38bfae94047cf9
mattiacoli <mattia.coli@aol.com>
2025-04-29 11:18:40 +0200

    Refactor CSS and DefaultLayout for improved structure and readability

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -8,7 +8,7 @@ export default function DefaultLayout() {
     <>
       <Header />
 
-      <main style={{ height: '100vh' }}>
+      <main >
         <div className="row bg-light">
           <div className="col-3 col-sm-2 d-flex justify-content-center" style={{ width: '120px', boxShadow: '2px 0px 0 var(--br-primary)' }}>
             <Sidebar />
